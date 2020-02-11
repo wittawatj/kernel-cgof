2581fd91f520f3a638448bc85c15cdbfb6f964e2
Wittawat Jitkrittum <wittawatj@gmail.com>
2020-02-11 19:14:18 +0100

    implement the FSCD power criterion

--- kcgof/cgoftest.py
+++ kcgof/cgoftest.py
@@ -57,38 +57,6 @@ class CGofTest(object):
         """
         raise NotImplementedError()
 
-class KSSDWitness(object):
-    """
-    Construct a callable witness function associated with the KSSD test.
-    The witness function is real-valued and is defined as 
-    v |-> || G(v) ||^2
-    where G is the RKHS-valued function such that its squared RKHS norm
-    defines the KSSD statistic. The witness is supposed to be a zero function
-    under H0. In practice, G has to be estimated from the data.
-    """
-    def __init__(self, p, k, l, X, Y):
-        """
-        p: an instance of UnnormalizedCondDensity
-        k: a kernel.Kernel object representing a kernel on X
-        l: a kernel.KCSTKernel object representing a kernel on Y
-        X, Y: torch tensors representing the data for X and Y
-        """
-        self.p = p
-        self.k = k
-        self.l = l
-        self.X = X
-        self.Y = Y
-
-    def __call__(self, at):
-        """
-        at: Torch tensor of size m x dx specifying m locations to evaluate 
-            the witness function. The witness function is evaluated at each
-            point separately.
-
-        Return: one-dimensional torch array of length m representing the
-            values of the witness function evaluated at these locations.
-        """
-        pass
 
 
 class KSSDTest(CGofTest):
@@ -161,15 +129,12 @@ class KSSDTest(CGofTest):
         return results
 
 
-    def compute_stat(self, X, Y, return_ustat_gram=False):
+    def _unsmoothed_ustat_kernel(self, X, Y):
         """
-        Compute n x the U-statistic estimator of KSSD.
-
-        return_ustat_gram: If true, then return the n x n matrix used to
-            compute the statistic 
+        Compute h_p((x,y), (x',y')) for (x,y) in X,Y.
+        Return an n x n Torch tensor.
         """
         n, dy = Y.shape
-        k = self.k
         l = self.l
         # n x dy matrix of gradients
         grad_logp = self.p.grad_log(X, Y)
@@ -186,8 +151,23 @@ class KSSDTest(CGofTest):
             C += (l.gradY_X(Y, Y, i).T * grad_logp_i).T
 
         h = L*gram_glogp + B + C + l.gradXY_sum(Y, Y)
+        return h
+
+    def compute_stat(self, X, Y, return_ustat_gram=False):
+        """
+        Compute n x the U-statistic estimator of KSSD.
+
+        return_ustat_gram: If true, then return the n x n matrix used to
+            compute the statistic 
+        """
+        n, dy = Y.shape
+        k = self.k
+        l = self.l
+        h = self._unsmoothed_ustat_kernel(X, Y)
         # smoothing 
         K = k.eval(X, X)
+        # TODO: Wittawat: K*h then sum, this is the same as forming a quadratic
+        # form.
         H = K*h
         # U-statistic
         ustat = (torch.sum(H) - torch.sum(torch.diag(H)) )/(n*(n-1))
@@ -196,6 +176,130 @@ class KSSDTest(CGofTest):
             return stat, H
         else:
             return stat
+
+class FSCDPowerCriterion(object):
+    """
+    Construct a callable power criterion and witness functions associated
+    with the FSCD test.
+    The witness function is real-valued and is defined as 
+    v |-> || G(v) ||^2
+    where G is the RKHS-valued function such that its squared RKHS norm
+    defines the KSSD statistic. The witness is supposed to be a zero function
+    under H0. In practice, G has to be estimated from the data.
+    """
+    def __init__(self, p, k, l, X, Y):
+        """
+        p: an instance of UnnormalizedCondDensity
+        k: a kernel.Kernel object representing a kernel on X
+        l: a kernel.KCSTKernel object representing a kernel on Y
+        X, Y: torch tensors representing the data for X and Y
+        """
+        self.p = p
+        self.k = k
+        self.l = l
+        self.X = X
+        self.Y = Y
+        self.kssdtest = KSSDTest(p, k, l)
+
+    def eval_witness(self, at):
+        """
+        Evaluate the biased estimate of the witness function of KSSD/FSCD.
+
+        at: Torch tensor of size m x dx specifying m locations to evaluate 
+            the witness function. The witness function is evaluated at each
+            point separately.
+
+        Return: one-dimensional torch array of length m representing the
+            values of the witness function evaluated at these locations.
+        """
+        # TODO: can be improved by vectorzing and avoiding the for loop. Later.
+        return self._eval_witness_loop(at)
+
+    def eval_power_criterion(self, at):
+        """
+        The power criterion is, by construction, a function of a set of test
+        locations. So there are two modes of operation.
+
+        at: If this is a Torch tensor of size J x dx, then evaluate the power
+            criterion by treating the whole input tensor as one set of test
+            locations. Return one scalar output.
+
+            If this is a Torch tensor of size m x J x d, then interpret this
+            as m sets of test locations to evaluate, and return m scalar
+            outputs in a one-dimensional Torch array.
+        """
+        dim = len(at.shape)
+        if dim == 2:
+            return self._point_power_criterion(V=at)
+        elif dim == 3:
+            # TODO: try to improve the computation of this part. Not trivial
+            # though.
+            m, J, dx = at.shape
+            pc_values = torch.zeros(m)
+            for i in range(m):
+                Vi = at[i]
+                pc_values=[i] = self._point_power_criterion(V=Vi)
+            return pc_values
+
+        else:
+            raise ValueError('at must be a 2d or a 3d tensor. Found at.shape = {}'.format(at.shape))
+
+    def _point_power_criterion(self, V):
+        """
+        Evaluate the power criterion at the set of J locations in V.
+        """
+        kssdtest = self.kssdtest
+        k = self.k
+
+        h = kssdtest._unsmoothed_ustat_kernel(self.X, self.Y)
+        n = h.shape[0]
+        J, dx = V.shape
+
+        # n x J
+        Phi = k.eval(self.X, V)
+        Kbar = Phi.matmul(Phi.T)/J
+        # standard deviation under H1.
+        hKbar = h*Kbar
+        sigma_V = 2.0*torch.std(torch.mean(h*Kbar, 1))
+
+        # compute biased FSCD = average of the witness values at the J
+        # locations
+        fscd_biased = torch.mean(hKbar)
+        power_cri = fscd_biased/sigma_V
+        return power_cri
+
+    # def _point_h1_std(self, V):
+    #     """
+    #     Evaluate the standard deviation of the the distribution of FSCD under H1.
+    #     Use V as the set of J test locations.
+    #     """
+    #     raise NotImplementedError()
+    
+    def _eval_witness_loop(self, at):
+        """
+        Same as eval_witness(.).
+        This is the version with a for loop.
+        Use eval_witness(.)
+        """
+        kssdtest = self.kssdtest
+        # TODO: h can be cached if needed. But it may consume a lot of memory
+        # (n x n)
+        h = kssdtest._unsmoothed_ustat_kernel(self.X, self.Y)
+        n = h.shape[0]
+        # remove bias (diagonal)
+        # h = h - torch.diagflat(torch.diag(h))
+        m, dx = at.shape
+        dy = self.Y.shape[1]
+        k = self.k
+        wit_values = torch.zeros(m)
+        for i in range(m):
+            loc_i = at[[i], :]
+            # n x 1
+            Phi = k.eval(self.X, loc_i)
+            # print(h.matmul(Phi.reshape(-1)).dot(Phi.reshape(-1))/n**2)
+            wit_values[i] = h.matmul(Phi.reshape(-1)).dot(Phi.reshape(-1))/(dy*n**2)
+        return wit_values
+
 
 class FSCDTest(KSSDTest):
     """
